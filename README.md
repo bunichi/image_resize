4c0be32a8b989f7c90f34cc67c1ebdf35c93e376
bun <sugita@bita.jp>
2017-03-23 21:16:03 +0900

    phpの関数を使ってリサイズするように修正

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # image_resize
+複数の画像ファイルを正方形にリサイズするプログラムです。
+画像の中心を基準に、トリミングを行って正方形にし、アスペクト比を保ってリサイズします。
+サポートする画像形式はpngまたはjpgのみです。
+ディレクトリ1階層分だけ掘り進んでリサイズします。（INDIRの直下にある画像はリサイズ対象外で、INDIR/ANYDIR/any_image.jpg がリサイズ対象です）
+
+・使い方
+$ php resize.php
+
+・リサイズ後の画像サイズは resize.php の以下の箇所を好きな値に変更可能です。
+define('OUTSIZE', 227);
+
+・入力画像の置き場所を resize.php の以下で設定し直してください。resize.php の置いてあるディレクトリが基準になります。
+define('INDIR', '../images_trim2/');
+
+・リサイズ後の画像は、配置ディレクトリ名、ファイル名、ファイル形式を保って以下に出力されます。
+入力：INDIR/ANYDIR/any_image.jpg
+出力：OUTDIR/{time}resized/ANYDIR/any_image.jpg
+出力先ディレクトリを変更する場合、resize.php の以下を編集してください。
+define('OUTDIR', './');
--- resize.php
+++ resize.php
@@ -3,45 +3,61 @@
 // 出力画像サイズ
 define('OUTSIZE', 227);
 
+// JPG画像圧縮度
+define('JPG_QUALITY', 75);
+
 // 入力ディレクトリ
-define('INDIR', './images_trim2/');
+define('INDIR', '../images_trim2/');
 
 // 出力ディレクトリ
 define('OUTDIR', './');
 /**************/
+
 $dirs = scandir(INDIR);
-$new_dir = OUTDIR . time() . 'resized';
-print($new_dir . 'に画像を出力します');
+$new_dir = OUTDIR . time() . 'resized/';
+print($new_dir . ' に画像を出力します');
 exec('mkdir -p ' . $new_dir);
 foreach($dirs as $dir){
-	print("\n" . $dir . 'に画像出力中');
 	if( ($dir != '.') && ($dir != '..') && !strpos($dir, '.')){
+		print("\n" . $new_dir . $dir . ' に画像出力中');
 		$files = scandir(INDIR . $dir);
 
 		if( count($files) ){
 			foreach($files as $file){
 				if(strpos($file, '.jpg') || strpos($file, '.png')){
-					exec('mkdir -p ' . $new_dir . '/' . $dir);
-					exec('mkdir -p ' . $new_dir . '/tmp');
-					$new_file = $new_dir . '/' . $dir . '/' . $file;
-					$old_file = INDIR . '/' . $dir . '/' . $file;
-					$tmp_file = $new_dir . '/tmp/' . $file;
+					exec('mkdir -p ' . $new_dir . $dir);
+					$new_file = $new_dir . $dir . '/' . $file;
+					$old_file = INDIR . $dir . '/' . $file;
+					$image_size = getimagesize($old_file);
+					$width = $image_size[0];
+					$height = $image_size[1];
 
-					$width = exec("identify -format '%w' " . $old_file);
-					$height = exec("identify -format '%h' " . $old_file);
-
-					// 短い辺を基準にして正方形にトリミング
+					// 短い辺を基準にする
 					if($width > $height){
+						$source = $height;
 						$pos_x = ($width - $height) / 2;
-						exec('convert -crop ' . $height . 'x' . $height . '+' . $pos_x . '+0 ' . $old_file . ' ' . $tmp_file);
+						$pos_y = 0;
 					}else{
+						$source = $width;
+						$pos_x = 0;
                         $pos_y = ($height - $width) / 2;
-                        exec('convert -crop ' . $width . 'x' . $width . '+0+' . $pos_y . ' ' . $old_file . ' ' . $tmp_file);
 					}
 
-					// リサイズ
-					exec('convert -resize ' . OUTSIZE . 'x' . OUTSIZE . ' ' . $tmp_file . ' ' . $new_file);
-					exec('rm -r ' . $new_dir . '/tmp');
+					// 画像のトリミングとリサイズを行う
+					$rect = imagecreatetruecolor(OUTSIZE, OUTSIZE);
+					$old_image = imagecreatefromstring(file_get_contents($old_file));
+					if(!imagecopyresampled($rect, $old_image, 0, 0, $pos_x, $pos_y, OUTSIZE, OUTSIZE, $source, $source)){
+						print($old_image . ' の画像リサンプリングに失敗しました');
+					}
+
+					// 元の画像形式に合わせてファイル出力
+					if(strpos($file, '.jpg')){
+						imagejpeg($rect, $new_file, JPG_QUALITY);
+					}else if(strpos($file, '.png')){
+						imagepng($rect, $new_file);
+					}else{
+						print("サポートされていない画像形式です");
+					}
 					echo('-');
 				}
 			}
